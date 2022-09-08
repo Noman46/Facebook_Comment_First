16f24dc5121983c8c28a39149c20e064afe8e492
Noman46 <nomanibrahim7@gmail.com>
2022-09-08 16:23:52 +0600

    add log in and comment

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,104 @@
+
+
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () {
+        return $('#username');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('button[type="submit"]');
+    }
+
+    get filterLanguage() {
+        return $('//div[@role="button" and @class="dropdown__handle"]')
+    }
+
+    get selectLanguage() {
+        return $('//input[@value="php"]')
+    }
+
+    // get learningContent() {
+    //     const elements = $$('//div[@class="card"]')
+    //     return elements.values.length
+    // }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    async clickOnFilter() {
+        await this.filterLanguage.click();
+    }
+
+    async clickDesireLanguage() {
+        
+        await this.selectLanguage.click()
+    }
+
+    async totalNumberOfLearningContent(cardNumber) {
+        let elements = await $(`(//img[@class="card__image"])[${cardNumber}]`)
+        await elements.scrollIntoView()
+        await elements.click()
+    }
+
+    async scrollThroughTheSection(sectionNumber) {
+        let sections = await $(`(//div[@class="vue--layout-step"]/div[@class="vue--layout-step__body"])[${sectionNumber}]`)
+        await sections.scrollIntoView()
+    }
+
+    async countTotalTickSign() {
+        let tickSigns = await $$('//div[@class="tableOfContents__checkbox tableOfContents__checkbox--read tableOfContents__checkbox--changed"]').length
+        console.log(`The number =========== ${tickSigns}`)
+    }
+    async clickOnLoginButton(){
+        let loginButton = await $('//div[@aria-label="Accessible login button"]')
+        await loginButton.click()
+    }
+
+    async loginToFaceBook(){
+        let userName = await $('//input[@aria-label="Email address or phone number"]')
+        let password = await $('//input[@aria-label="Password"]')
+        let loginButton = await $('//button[@name="login"]')
+
+        await userName.setValue("nomanibrahim420@gmail.com")
+        await password.setValue("nomanfacebook46")
+        await loginButton.click()
+    }
+
+    async putTheComment () {
+        let commentBox = await $('//div[@aria-label="Write a comment"]')
+        await commentBox.setValue("CEFALO")
+    }
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open();
+    }
+
+    openFacebook () {
+        return super.openFacebook();
+    }
+
+
+}
+
+module.exports = new LoginPage();
---
+++ test/specs/example.e2e.js
@@ -0,0 +1,45 @@
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+
+describe('My Login application', () => {
+    // it('should login with valid credentials', async () => {
+    //     await LoginPage.open();
+    //     await browser.pause(2000)
+    //     await LoginPage.clickOnFilter();
+    //     await browser.pause(2000)
+    //     await LoginPage.clickDesireLanguage()
+    //     await browser.pause(3000)
+    //     await LoginPage.clickOnFilter();
+    //     await browser.pause(5000)
+    //     await LoginPage.totalNumberOfLearningContent(1)
+    //     await browser.pause(3000)
+    //     await LoginPage.scrollThroughTheSection(1)
+    //     await browser.pause(7000)
+    //     await LoginPage.scrollThroughTheSection(2)
+    //     await browser.pause(7000)
+    //     await LoginPage.scrollThroughTheSection(3)
+    //     await browser.pause(7000)
+    //     await LoginPage.scrollThroughTheSection(4)
+    //     await browser.pause(7000)
+    //     await LoginPage.scrollThroughTheSection(1)
+    //     await browser.pause(2000)
+    //     await LoginPage.countTotalTickSign()
+    //     await browser.pause(3000)
+    //     await browser.saveScreenshot('test/screenshot/com.png');
+    //     //await browser.back()
+        
+
+        
+    // });
+
+    it('should login facebook', async () => {
+        await LoginPage.openFacebook();
+        await LoginPage.clickOnLoginButton()
+        await LoginPage.loginToFaceBook()
+        await browser.pause(15000)
+        await LoginPage.putTheComment()
+        await browser.pause(15000)
+    });
+});
+
+
